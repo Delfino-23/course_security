ebe60611670c1f22a79026c558dd2cd77bca2706
Delfino-23 <delfino.carlosedu@gmail.com>
2026-04-03 16:07:28 -0300

    create models

--- src/main/java/com/course/security/models/Course.java
+++ src/main/java/com/course/security/models/Course.java
@@ -20,4 +20,28 @@ public class Course {
 
     @Column(name = "description", length = 1000)
     private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
